a43dfa8679ae238173f06de2742e2f01990c65b6
eduardozuppodev <zuppo.edu@gmail.com>
2021-06-09 08:17:05 -0300

    .

--- clone_dropbox/src/components/Section/styles.ts
+++ clone_dropbox/src/components/Section/styles.ts
@@ -1,8 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  
-`;
+  --padding-top: 100px;
+  --padding-bottom: 120px;
+  --heading-font-size: 32px;
+  --content-width: 100%;
 
+  &.blue {
+    --bg-color: var(--color-primary);
+    --text-color: var(--color-tertiary);
+  }
+`;
 
 export const Content = styled.div``;
