5219722b3a45274799ea1f390c9070a353eca08d
eduardozuppodev <zuppo.edu@gmail.com>
2021-06-10 08:41:43 -0300

    .

--- clone_dropbox/src/App.tsx
+++ clone_dropbox/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import GlobalStyles from './styles/GlobalStyles';
 import Section from './components/Section';
+import SideMenu from './components/SideMenu';
+import MenuForm from './components/MenuForm';
 
 import data from './data';
 
@@ -15,24 +17,28 @@ function App() {
       />
       <Section
         variant="beige"
-        title={data[0].title}
-        description={data[0].description}
+        title={data[1].title}
+        description={data[1].description}
       />
       <Section
         variant="blue"
-        title={data[0].title}
-        description={data[0].description}
+        title={data[2].title}
+        description={data[2].description}
       />
       <Section
-        variant="blue"
-        title={data[0].title}
-        description={data[0].description}
+        variant="white"
+        title={data[3].title}
+        description={data[3].description}
       />
       <Section
-        variant="blue"
-        title={data[0].title}
-        description={data[0].description}
+        variant="black"
+        title={data[4].title}
+        description={data[4].description}
       />
+
+      <SideMenu>
+        <MenuForm />
+      </SideMenu>
 
       <GlobalStyles />
     </>
